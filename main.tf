35c2f67a62acdebcd353ae699edda8662f57b330
rayhaan <rayhaan10@gmail.com>
2021-05-11 16:15:31 -0500

    added random pet count

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "hashicorp/aws"
+      version = ">= 0.15"
+    }
+  }
+}
+
+resource "random_pet" "rayray" {
+  prefix = var.prefix
+  count  = 3
+}
+
+#prefix = "james"
---
+++ variables.tf
@@ -0,0 +1,8 @@
+#variable "prefix" {
+#  default = "rayhaan"
+#}
+
+variable "names" {
+  type    = list(string)
+  default = ["Monkey_man", "lion_man", "Zebra_man"]
+}
