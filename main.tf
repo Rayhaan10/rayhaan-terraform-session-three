660b0887fd9016bfe6033df31d6bf61d113dd2ab
rayhaan <rayhaan10@gmail.com>
2021-05-12 14:43:45 -0500

    added length

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@
 resource "random_pet" "rayray" {
   count  = 3
   prefix = var.prefix[count.index]
+  length = var.length[count.index]
 }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,8 @@ variable "prefix" {
   type    = list(string)
   default = ["panda", "banana", "chipotle"]
 }
+
+variable "length" {
+  type    = list(number)
+  default = [2, 3, 4]
+}
