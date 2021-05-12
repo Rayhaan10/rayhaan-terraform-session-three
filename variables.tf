959a603dee0d9b54b4f93085d44c851d266c76ef
rayhaan <rayhaan10@gmail.com>
2021-05-12 14:14:17 -0500

    added list

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 #all variabls will be here
 
 variable "prefix" {
-  type = map(string)
-  default = {
-    "0" = "panda"
-    "1" = "banana"
-    "2" = "chipotle"
-  }
+  type    = list(string)
+  default = ["panda", "banana", "chipotle"]
 }
