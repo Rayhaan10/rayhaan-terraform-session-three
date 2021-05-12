832f4e345ab550c21c0964bc123a68e47a1e0b16
Rayhaan10 <77302886+Rayhaan10@users.noreply.github.com>
2021-05-11 19:01:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,1 @@
-terraform {
-  required_providers {
-    mycloud = {
-      source  = "hashicorp/aws"
-      version = ">= 0.15"
-    }
-  }
-}
 
-resource "random_pet" "rayray" {
-  prefix = var.prefix
-  count  = 3
-}
-
-#prefix = "james"
