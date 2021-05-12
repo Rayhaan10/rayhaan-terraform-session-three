775b692c1f30b8a9e98891abbeca5630beee8a10
rayhaan <rayhaan10@gmail.com>
2021-05-12 10:46:59 -0500

    added constraint for version

--- versions.tf
+++ versions.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 0.11.0"
   required_providers {
     mycloud = {
+      version = ">= 2.7.0"
       source  = "hashicorp/aws"
-      version = ">= 0.11.0"
     }
   }
 }
